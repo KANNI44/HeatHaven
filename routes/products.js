7025aa92059d12d6d00c2b7550f0fb8484f418ab
KANNI44 <kanishakchoudhary268@gmail.com>
2026-04-07 14:44:23 +0530

    ..

--- routes/products.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/productsController');
-
-router.get('/', ctrl.getProducts);
-router.get('/:legacyId', ctrl.getProduct);
-
-module.exports = router;
-
